868be79f7d29ba265c878da1e3dd79b2cd978bc4
arranger1044 <arranger1044@aim.com>
2020-09-18 15:00:48 -0700

    Refining test script

--- test/test_load_save.py
+++ test/test_load_save.py
@@ -191,6 +191,8 @@ train_x = torch.from_numpy(train_x).to(torch.device(device))
 valid_x = torch.from_numpy(valid_x).to(torch.device(device))
 test_x = torch.from_numpy(test_x).to(torch.device(device))
 
+
+use_em = False
 rg = make_region_graph(structure="poon-domingos",
                       height=28, width=28,
                       pd_pieces=[4], depth=None, n_repetitions=None)
@@ -200,7 +202,7 @@ einet = make_einet(region_graph=rg,
                n_sums=10,
                n_input_dists=10,
                exp_fam="binomial", exp_fam_args={'N': 255},
-               use_em=False,
+               use_em=use_em,
                # em_freq=1, em_stepsize=0.05
                em_freq=None, em_stepsize=None
                )
@@ -231,21 +233,23 @@ print("pre-train   train LL {}   valid LL {}   test LL {}".format(
 ######################################
 # Save untrained
 
-out_path = './saved-pretrained-state'
-save_einet_state(einet, rg, out_path=out_path)
-einet_p, graph_p = load_einet_state(out_path, graph=rg,
-                                    n_vars=28*28,
-               n_classes=1,
-               n_sums=10,
-               n_input_dists=10,
-               exp_fam="binomial", exp_fam_args={'N': 255},
-               use_em=False,
-               # em_freq=1, em_stepsize=0.05
-               em_freq=None, em_stepsize=None)
+# out_path = './saved-pretrained-state'
+# save_einet_state(einet, rg, out_path=out_path)
+# einet_p, graph_p = load_einet_state(out_path, graph=rg,
+#                                     n_vars=28*28,
+#                n_classes=1,
+#                n_sums=10,
+#                n_input_dists=10,
+#                exp_fam="binomial", exp_fam_args={'N': 255},
+#                use_em=False,
+#                # em_freq=1, em_stepsize=0.05
+#                em_freq=None, em_stepsize=None)
 
-# out_path = './saved-pretrained'
-# save_einet(einet, rg, out_path=out_path)
-# einet_p, graph_p = load_einet(out_path)
+out_path = './saved-pretrained'
+save_einet(einet, rg, out_path=out_path)
+einet_p, graph_p = load_einet(out_path)
+
+
 
 ##### evaluate
 einet_p.eval()
@@ -257,46 +261,65 @@ print("pre-train (after loading)   train LL {}   valid LL {}   test LL {}".forma
     valid_ll / valid_N,
     test_ll / test_N))
 
-0/0
 
-einet.train()
 #####
+einet.train()
 
-idx_batches = torch.randperm(train_N, device=device).split(batch_size)
+optimizer = None
+if not use_em:
+    optimizer = torch.optim.Adam(einet.parameters(), lr=1e-2)
 
-total_ll = 0.0
-for idx in idx_batches:
-    batch_x = train_x[idx, :]
-    outputs = einet.forward(batch_x)
-    ll_sample = EinsumNetwork.log_likelihoods(outputs)
-    log_likelihood = ll_sample.sum()
-    log_likelihood.backward()
+n_epochs = 5
+for epoch in range(n_epochs):
+    idx_batches = torch.randperm(train_N, device=device).split(batch_size)
 
-    einet.em_process_batch()
-    total_ll += log_likelihood.detach().item()
+    total_ll = 0.0
+    for idx in idx_batches:
+        batch_x = train_x[idx, :]
+        outputs = einet.forward(batch_x)
+        ll_sample = EinsumNetwork.log_likelihoods(outputs)
+        log_likelihood = ll_sample.sum()
+        
 
-einet.em_update()
+        if use_em:
+            log_likelihood.backward()
+            einet.em_process_batch()
+        else:
+            optimizer.zero_grad()
+            objective = -log_likelihood
+            objective.backward()
+            optimizer.step()
+            
+        total_ll += log_likelihood.detach().item()
+    print(f"done epoch {epoch+1}/{n_epochs}")
+
+    if use_em:
+        einet.em_update()
 
 
 # evaluate log-likelihoods
 einet.eval()
-train_ll_before = EinsumNetwork.eval_loglikelihood_batched(einet, train_x, batch_size=batch_size)
-valid_ll_before = EinsumNetwork.eval_loglikelihood_batched(einet, valid_x, batch_size=batch_size)
-test_ll_before = EinsumNetwork.eval_loglikelihood_batched(einet, test_x, batch_size=batch_size)
-
-
-
-
 # evaluate log-likelihoods on re-loaded model
 train_ll = EinsumNetwork.eval_loglikelihood_batched(einet, train_x, batch_size=batch_size)
 valid_ll = EinsumNetwork.eval_loglikelihood_batched(einet, valid_x, batch_size=batch_size)
 test_ll = EinsumNetwork.eval_loglikelihood_batched(einet, test_x, batch_size=batch_size)
 print()
-print("Log-likelihoods before saving --- train LL {}   valid LL {}   test LL {}".format(
+print("post-training (before saving) --- train LL {}   valid LL {}   test LL {}".format(
         train_ll / train_N,
         valid_ll / valid_N,
         test_ll / test_N))
-print("Log-likelihoods after saving  --- train LL {}   valid LL {}   test LL {}".format(
-        train_ll / train_N,
-        valid_ll / valid_N,
-        test_ll / test_N))
+
+
+save_einet(einet, rg, out_path=out_path)
+einet_p, graph_p = load_einet(out_path)
+
+##### evaluate
+einet_p.eval()
+train_ll = EinsumNetwork.eval_loglikelihood_batched(einet_p, train_x, batch_size=batch_size)
+valid_ll = EinsumNetwork.eval_loglikelihood_batched(einet_p, valid_x, batch_size=batch_size)
+test_ll = EinsumNetwork.eval_loglikelihood_batched(einet_p, test_x, batch_size=batch_size)
+print("pre-train (after re-loading)   train LL {}   valid LL {}   test LL {}".format(
+    train_ll / train_N,
+    valid_ll / valid_N,
+    test_ll / test_N))
+
